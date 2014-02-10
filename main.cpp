ed7cd6c7370a01849e025f3de36f2cdb7c281282
Rodney Lyons <therodlyons@gmail.com>
2014-02-10 14:40:14 -0500

    created cities.txt file, main updated with various touring functionality

--- main.cpp
+++ main.cpp
@@ -1,24 +1,85 @@
+/* LANGUAGE: C++ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <deque>
 #include <algorithm>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+#include <unordered_set>
 
 const int num_cities_c = 25;
 
 using namespace std;
 
-// swap every ith and jth city 
-deque<vector<int>> citySwapSuccessors(const vector<int> & tour)
+void printTour(const vector<double> & tour);
+
+// generates a tour randomly from [0, num_cities)
+vector<double> genRandomTour(const int & num_cities)
 {
-	deque<vector<int>> successors;
+	unordered_set<double> cities_generated;	
+	vector<double> tour;
+	tour.reserve(num_cities);
+
+	srand(time(NULL));	
+
+	while (tour.size() < num_cities)
+	{
+		int gen_city = rand() % num_cities;	
+
+		// new city generated! add to both containers
+		if (cities_generated.find(gen_city) == cities_generated.end())
+		{
+			cities_generated.insert(gen_city);
+			tour.push_back(gen_city);
+		}
+	}		
+
+	return tour;
+}
+
+vector<double> genTour(const int & num_cities)
+{
+	vector<double> tour(num_cities, -1);
+
+	for (int i = 0; i < num_cities; i++)
+		tour[i] = i;
+
+	return tour;
+}
+
+/* objective function : cost of a tour */
+double getTourCost(const vector<vector<double>> & costs, const vector<double> & tour)
+{
+	double totalCost = 0;
+	cout << endl;
+
+	// for each consecutive city i and j add distance between them to total cost
+	for (int i = 0; i < tour.size() - 1; i++)
+	{
+		cout << i << " to " << i + 1 << " = " << costs[tour[i]][tour[i + 1]] << endl;
+		totalCost += costs[tour[i]][tour[i + 1]];
+	}
+
+	// last city back to first city
+	totalCost += costs[tour.back()][tour[0]];
+
+	return totalCost;
+}
+
+// swap every ith and jth city 
+deque<vector<double>> citySwapSuccessors(const vector<double> & tour)
+{
+	deque<vector<double>> successors;
 
 	for (int i = 0; i < tour.size() - 1; i++)
 	{
 		for (int j = i + 1; j < tour.size(); j++)
 		{
-			vector<int> successor = tour;
+			vector<double> successor = tour;
 			swap(successor[i], successor[j]);	
 			successors.push_back(successor);
 		}
@@ -28,44 +89,64 @@ deque<vector<int>> citySwapSuccessors(const vector<int> & tour)
 }
 
 // reverse all substrings of length > 1 
-deque<vector<int>> twoOptSuccessors(const vector<int> & tour)
+vector<double> genSubstring(const vector<double> & tour, int sub_start, int sub_end);
+deque<vector<double>> twoOptSuccessors(const vector<double> & tour)
 {
-	deque<vector<int>> successors;
+	deque<vector<double>> successors;
 
-	for (int i = 2; i < tour.size(); i++)
-	{
-		
-	}
+	for (int i = 0; i < tour.size() - 1; i++)
+		for (int j = i + 1; j < tour.size(); j++)	
+			successors.push_back(genSubstring(tour, i, j));
 
 	return successors;
 }
 
-vector<vector<int>> readCityMatrix(const string & file_name)
+vector<double> genSubstring(const vector<double> & tour, int sub_start, int sub_end)
 {
-	ifstream fin(file_name);
+	vector<double> successor = tour;
 
+	while (sub_start < sub_end)
+	{
+		swap(successor[sub_start], successor[sub_end]);	
+		++sub_start;
+		--sub_end;
+	}	
+
+	return successor;
+}
+
+//vector<vector<double>> readCityMatrix(const string & file_name)
+vector<vector<double>> readCityMatrix(ifstream & fin)
+{
 	if (!fin)
 	{
-		cerr << "error on opening file: '" << file_name << "'" << endl;
+		cout << "bad file passed to readCityMatrix function! exiting..." << endl;
 		exit(1);
 	}
 
-	vector<vector<int>> matrix (num_cities_c, vector<int> (num_cities_c, 0));
+	vector<vector<double>> matrix (num_cities_c, vector<double> (num_cities_c, -1));
 
 	for (int i = 0; i < num_cities_c; i++)
 	{
 		for (int j = 0; j < num_cities_c; j++)
 		{
-			int val;
+			double val;
 			fin >> val;
 
 			if (!fin)
 			{
-				cerr << "error on opening file: '" << file_name << "'" << endl;
+				cerr << "error when reading file!" << endl;
 				exit(1);
 			}
 
 			matrix[i][j] = val;
+
+			// ignore commas
+			fin.ignore(256, ',');
+
+			// edge case that i'm too sick to deal with right now
+			if (fin.eof())
+				break;
 		}
 	}
 
@@ -74,7 +155,8 @@ vector<vector<int>> readCityMatrix(const string & file_name)
 
 int main(int argc, char * argv[])
 {	
-	vector<int> vec {1, 2, 3, 4};
+	/* debugging purposes, prints successors using City Swap and 2-Opt successor functions for tour 1-2-3-4 
+	vector<double> vec {1, 2, 3, 4};
 
 	//auto res = citySwapSuccessors(vec);
 	auto res = twoOptSuccessors(vec);
@@ -85,18 +167,38 @@ int main(int argc, char * argv[])
 			cout << i << " ";
 		cout << endl;
 	}
+	*/
 
-	auto matrix = readCityMatrix("cities.csv");
+	// read-in cost symmetric matrix
+	ifstream fin("cities.txt");
+	auto costs = readCityMatrix(fin);
+	fin.close();
 
-	for (const auto & row : matrix)
+	//* debugging purposes, prints cost matrix 
+	cout << "cities.csv : " << endl;
+	for (const auto & row : costs)
 	{
 		for (const auto & col : row)
-		{
-			cout << col << " ";	
-		}	
+			cout << setw(3) << col << " ";	
 
 		cout << endl;
 	}
 
+	auto x = genTour(num_cities_c);
+	auto y = genRandomTour(num_cities_c);	
+
+	printTour(genTour(3));
+	printTour(y);	
+
+	cout << "(0, 1, 2) tour cost = " << getTourCost(costs, genTour(3)) << endl;
+
 	return 0;
+}
+
+void printTour(const vector<double> & tour)
+{
+	for (const auto & city : tour)
+		cout << city << " ";
+
+	cout << endl;
 }
